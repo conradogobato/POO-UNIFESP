d2dc54da12d62c72906550aa2311b3afda70275d
conradogobato <conradogob07@gmail.com>
2023-08-28 20:17:34 -0300

    feat: added new rent bike code

---
+++ Sisitema aluguel de bikes 2/src/index.ts
@@ -0,0 +1,15 @@
+import { Bike } from "./bike";
+import { Rent } from "./rent";
+import { User } from "./user";
+
+const bike = new Bike('Xray', 'road', 46, 10, 5)
+
+const user = new User('Rafael', 'rafael@gmail.com', '02198')
+
+const today = new Date()
+
+const twoDaysfromToday = new Date()
+twoDaysfromToday.setDate(twoDaysfromToday.getDate()+2)
+
+
+const rentOne = Rent.create([], bike, user, today, twoDaysfromToday)
